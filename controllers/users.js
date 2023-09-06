c50bbff6675fdb1d595164cb01476b98e23e5f1c
ColbyCabrera <gdthyispro@gmail.com>
2023-09-06 13:31:34 -0400

    Add user routes

--- controllers/users.js
+++ controllers/users.js
@@ -7,12 +7,19 @@ const User = require("../models/user");
 //const bcrypt = require("bcryptjs");
 
 exports.users = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+
+  res.json({ users }); 
+});
+
+exports.create_user = asyncHandler(async (req, res) => {
+  console.log(req.body.email);
   const user = new User({
-    email: "test@t",
-    password: "testpass",
+    email: req.body.email,
+    password: req.body.password,
   });
 
   await user.save();
 
-  res.json({ user }); 
-});
+  res.json(user);
+})
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,17 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const user_controller = require("../controllers/users");
 
 /* GET users listing. */
 router.get('/', user_controller.users);
+
+router.post('/', user_controller.create_user);
+/*
+router.get("/:userId", user_controller.get_user);
+
+router.put("/:userId", user_controller.update_user);
+
+router.delete("/:userId", user_controller.delete_user);
+*/
 
 module.exports = router;

dea36b4ae4bf4b498f2c69deb348d5c544b510fe
ColbyCabrera <gdthyispro@gmail.com>
2023-09-20 14:00:07 -0400

    Add function to create blog elements

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -4,10 +4,36 @@ async function newData() {
       mode: "cors",
     });
     const data = await response.json();
-    console.log(data);
+    createBlogs(data.blogs);
   } catch (error) {
     console.log(error);
   }
+}
+
+function createBlogs(blogs) {
+  const blogsContainer = document.getElementById("blogs-container");
+  blogs.forEach((blog) => {
+    const blogDiv = document.createElement("div");
+    const innerDiv = document.createElement("div");
+    const blogTitle = document.createElement("h3");
+    const blogText = document.createElement("p");
+    const blogAuthor = document.createElement("p");
+
+    blogDiv.className = "blog";
+    blogTitle.className = "blog-title";
+    blogText.className = "blog-text";
+    blogAuthor.className = "blog-author";
+
+    blogTitle.textContent = blog.title;
+    blogText.textContent = blog.text;
+    blogAuthor.textContent = blog.author.email
+
+    blogsContainer.appendChild(blogDiv);
+    blogDiv.appendChild(innerDiv);
+    blogDiv.appendChild(blogAuthor);
+    innerDiv.appendChild(blogTitle);
+    innerDiv.appendChild(blogText);
+  });
 }
 
 newData();
